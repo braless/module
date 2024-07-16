5321c5878fde645f5d7e2afc05687764d470bfa9
崔彦超 <1060846413@qq.com>
2024-07-16 23:31:00 +0800

    增加养基宝Vip

--- js/yangJiBao.js
+++ js/yangJiBao.js
@@ -25,6 +25,33 @@
  *******************************/
 
 
-var body ={"code":200,"message":"SUCCESS","timestamp":1721138195,"data":{"has_fund_hold":true,"nickname":"神经蛙","has_fund_option":true,"open_free_vip_sign":true,"is_pay":true,"medal":[],"is_app_user":true,"id":202691,"gender":1,"token":"9aaf7fde-976b-4d7c-b3ea-f1f12374583a","roles":[],"phone":"","subscribe_status":1,"vip_expiry_date":"2032-03-20","avatar":"http:\/\/imgcdn.yangjibao.com\/avatar\/316a5\/x8vSAyTPtjmgps9kx8HmzcyUOM.jpeg","vip_label":true,"created_at":"2021-08-04 12:37:44","has_stock_hold":false,"has_stock_option":false,"show_bkxh":false,"is_visitor":true}}
+var body = {
+    "code": 200,
+    "message": "SUCCESS",
+    "timestamp": 1721138195,
+    "data": {
+        "has_fund_hold": true,
+        "nickname": "神经蛙",
+        "has_fund_option": true,
+        "open_free_vip_sign": true,
+        "is_pay": true,
+        "medal": [],
+        "is_app_user": true,
+        "id": 202691,
+        "gender": 1,
+        "token": "9aaf7fde-976b-4d7c-b3ea-f1f12374583a",
+        "roles": [],
+        "phone": "",
+        "subscribe_status": 1,
+        "vip_expiry_date": "2999-03-20",
+        "avatar": "http:\/\/imgcdn.yangjibao.com\/avatar\/316a5\/x8vSAyTPtjmgps9kx8HmzcyUOM.jpeg",
+        "vip_label": true,
+        "created_at": "2021-08-04 12:37:44",
+        "has_stock_hold": false,
+        "has_stock_option": false,
+        "show_bkxh": false,
+        "is_visitor": true
+    }
+}
 
 $done({body});
