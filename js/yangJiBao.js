f754caa6f6f0884d10641dbfc4a7ecf331639dd2
崔彦超 <1060846413@qq.com>
2024-07-16 22:10:27 +0800

    增加养基宝Vip

---
+++ js/yangJiBao.js
@@ -0,0 +1,24 @@
+/*******************************
+
+ 脚本名称: 养基宝
+ 下载地址：https://is.gd/63j7aM
+ 软件版本：3.7.1
+ 脚本作者：彭于晏💞
+ 更新时间：2023-8-18
+ 问题反馈：QQ+89996462
+ QQ会员群：779392027💞
+ TG反馈群：https://t.me/plus8889
+ TG频道群：https://t.me/py996
+ 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+ 更多资源请微信搜索小程序【屌丝博客】
+
+ *******************************/
+
+//var body = {"validated":true,"key":"KV66M9P3WJ","create_time":"2099-02-02 00:00:00"}
+var objc = JSON.parse($response.body);
+objc.data.is_pay = true;
+objc.data.open_free_vip_sign=true;
+objc.data.subscribe_status=0;
+objc.data.vip_expiry_date="2099-02-02"
+objc.data.vip_label=true;
+$done({body : JSON.stringify(objc)});
