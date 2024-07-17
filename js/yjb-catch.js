3759d6fc9068092a6a2288f4c33aa426746197fa
崔彦超 <1060846413@qq.com>
2024-07-17 11:59:31 +0800

    增加养基宝Vip

--- js/yjb-catch.js
+++ js/yjb-catch.js
@@ -11,7 +11,6 @@ function run() {
     const text = $response.body;
     const body = JSON.parse(text);
     const yjbData= body.data
-    $.notify("养基宝", "请求地址:"+url, "")
     try {
         if (url.indexOf('fund_buy_ranking') > -1 ) {
                 let data = { "type": "fund_buy_ranking", "value": yjbData}
