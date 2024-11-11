e793c2b22b5d3ac845965b114ab0505e6f4ccd38
崔彦超 <1060846413@qq.com>
2024-11-11 17:14:03 +0800

    增加青藤之恋个人主页解析

--- js/yjb-crack.js
+++ js/yjb-crack.js
@@ -4,7 +4,7 @@ objc.data.nickname = "超级SVIP";
 objc.data.is_pay = true;
 objc.data.open_free_vip_sign = true;
 objc.data.subscribe_status = 0;
-objc.data.vip_expiry_date = "2099-12-31"
+objc.data.vip_expiry_date = "2299-12-31"
 objc.data.vip_label = true;
 objc.data.created_at = "2020-01-01 01:01:01"
 $done({body: JSON.stringify(objc)});
